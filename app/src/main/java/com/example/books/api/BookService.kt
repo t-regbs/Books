0915bae8828fb2d182fde3a45dc4e10d28930e04
Timilehin <aregbestimi@gmail.com>
2020-05-11 01:11:45 +0100

    added retrofit service

--- app/src/main/java/com/example/books/api/BookService.kt
+++ app/src/main/java/com/example/books/api/BookService.kt
@@ -1,10 +1,91 @@
 package com.example.books.api
 
+import android.util.Log
+import com.example.books.ApiUtil
+import com.example.books.model.Book
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+private const val TAG = "BookService"
+private const val TITLE = "intitle:"
+private const val AUTHOR = "inauthor:"
+private const val PUBLISHER = "inpublisher:"
+private const val ISBN = "isbn:"
+
+fun searchBooks(
+        service: BookService,
+        title: String,
+        author: String?,
+        publisher: String?,
+        isbn: String?,
+        max: Int,
+        key: String,
+        onSuccess: (repos: List<Book>) -> Unit,
+        onError: (error: String) -> Unit
+) {
+    Log.d(TAG, "title: $title, max: $max")
+    val sb = StringBuilder()
+    if (title.isNotEmpty()) sb.append("$TITLE$title+")
+    if (!author.isNullOrBlank()) sb.append("$AUTHOR$author+")
+    if (!publisher.isNullOrBlank()) sb.append("$PUBLISHER$publisher+")
+    if (!isbn.isNullOrBlank()) sb.append("$ISBN$isbn+")
+    sb.setLength(sb.length - 1)
+    val apiQuery = sb.toString()
+
+    service.searchBooks(apiQuery, key, max).enqueue(
+            object :Callback<BookSearchResponse> {
+                override fun onFailure(call: Call<BookSearchResponse>, t: Throwable) {
+                    Log.d(TAG, "fail to get data")
+                    onError(t.message ?: "unknown error")
+                }
+
+                override fun onResponse(call: Call<BookSearchResponse>,
+                                        response: Response<BookSearchResponse>) {
+                    Log.d(TAG, "got a response $response")
+                    if (response.isSuccessful) {
+                        val books = response.body()?.items ?: emptyList()
+                        onSuccess(books)
+                    } else {
+                        onError(response.errorBody()?.string() ?: "Unknown error")
+                    }
+                }
+
+            }
+    )
+}
+
 interface BookService {
 
-    @GET
-    fun searchBooks(@Query("q") title: String)
+    @GET("volumes")
+    fun searchBooks(
+            @Query("q") query: String,
+            @Query("key") apiKey: String,
+            @Query("maxResults") max: Int
+    ): Call<BookSearchResponse>
+
+    companion object {
+        private const val BASE_URL = "https://www.googleapis.com/books/v1/"
+
+        fun create(): BookService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(logger)
+                    .build()
+            return Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(BookService::class.java)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/books/model/Book.kt
@@ -0,0 +1,12 @@
+package com.example.books.model
+
+data class Book(
+        val id: String,
+        val title: String,
+        val subTitle: String,
+        val authors: String,
+        val publisher: String,
+        val publishedDate: String,
+        val description: String,
+        val thumbnail: String
+)
