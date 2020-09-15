a3910238e020726efd7b808a9c16df3e868222cc
Timilehin <aregbestimi@gmail.com>
2020-09-15 23:34:27 +0100

    small change

--- app/src/main/java/com/example/books/data/repository/BooksRemoteMediator.kt
+++ app/src/main/java/com/example/books/data/repository/BooksRemoteMediator.kt
@@ -38,23 +38,14 @@ class BooksRemoteMediator(
             LoadType.PREPEND -> {
                 Timber.d("PREPEND")
                 val remoteKeys = getRemoteKeyForFirstItem(state)
-                if (remoteKeys == null) {
-                    // The LoadType is PREPEND so some data was loaded before,
+                    ?: // The LoadType is PREPEND so some data was loaded before,
                     // so we should have been able to get remote keys
                     // If the remoteKeys are null, then we're an invalid state and we have a bug
                     throw InvalidObjectException("Remote key and the prevKey should not be null")
-                }
                 // If the previous key is null, then we can't request more data
-                val prevKey = remoteKeys.prevKey
-                if (prevKey == null) {
-                    return MediatorResult.Success(endOfPaginationReached = true)
-                }
+                val prevKey = remoteKeys.prevKey ?: return MediatorResult.Success(endOfPaginationReached = true)
                 remoteKeys.prevKey
-//                val remoteKey = getRemoteKeyForFirstItem(state)
-//                    ?: throw InvalidObjectException("Something went wrong.")
-//                remoteKey.prevKey
-//                    ?: return MediatorResult.Success(endOfPaginationReached = true)
-//                remoteKey.prevKey
+
             }
             LoadType.APPEND -> {
                 Timber.d("APPEND")
